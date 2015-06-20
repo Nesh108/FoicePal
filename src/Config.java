7ec44e819c02025ac8c79c1234b00620a6545e16
nesh <nesh@evildell>
2015-06-20 13:51:38 +0200

    initial mainPanel GUI

--- src/Config.java
+++ src/Config.java
@@ -1,6 +1,11 @@
 
 public class Config {
 	static public boolean DEBUG = true;
+
+	static public int WIDTH = 1366;
+	static public int HEIGTH = 768;
+	
+	/*// Final Dimensions
 	static public int WIDTH = 1920;
-	static public int HEIGTH = 1200;
+	static public int HEIGTH = 1200;*/
 }
--- src/GUI.java
+++ src/GUI.java
@@ -1,3 +1,10 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -5,8 +12,16 @@ import javax.swing.JPanel;
 public class GUI {
 	
 	// GUI Components
+	// Frames
 	private JFrame mainFrame;
+
+	// Panels
 	private JPanel mainPanel;
+	private JPanel botPanel;
+	
+	// Buttons
+	private JButton newCustomerButton;
+	private JButton fpCustomerButton;
 	
 	public GUI(){
 		// Main Frame
@@ -16,15 +31,54 @@ public class GUI {
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mainFrame.setLayout(null);
 		
-		// Panels
+		///// Panels ////////////////////////////////////////////////
 		// Main Panel
 		mainPanel = new JPanel();
 		mainPanel.setBounds(0, 0, Config.WIDTH, Config.HEIGTH);
 		mainPanel.setLayout(null);
 		
+		// Bot Panel
+		botPanel = new JPanel();
+		botPanel.setBounds(64, 5, 597, 604);
+		botPanel.setBorder(BorderFactory.createLineBorder(Color.GRAY, 1, true));
+		botPanel.setLayout(null);
+		
+		///// Buttons ////////////////////////////////////////////////
+		// New Customer Button
+		newCustomerButton = new JButton("New Customer");
+		newCustomerButton.setBounds(766, 476,389, 93);
+		newCustomerButton.setFont(new Font("Arial", Font.PLAIN, 40));
+		newCustomerButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+        });
+		
+		// FP Customer Button
+		fpCustomerButton = new JButton("FoicePal Customer");
+		fpCustomerButton.setBounds(766, 476,389, 93);
+		fpCustomerButton.setFont(new Font("Arial", Font.PLAIN, 40));
+		fpCustomerButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	
+            }
+
+        });
+		
+		// Adding components to main panel
+		mainPanel.add(botPanel);
+		mainPanel.add(newCustomerButton);
+		mainPanel.add(fpCustomerButton);
+		
 		// Adding components to main window
 		mainFrame.add(mainPanel);
-		
+
 		mainFrame.setVisible(true);
 	}
 
